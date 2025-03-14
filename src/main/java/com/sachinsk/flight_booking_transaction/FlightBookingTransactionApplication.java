887e72effa5cc5d058065ddea30e4b4816383bf6
Sachin <sachinkaushikofficial@gmail.com>
2025-03-14 17:17:07 +0530

    Initial commit

---
+++ src/main/java/com/sachinsk/flight_booking_transaction/FlightBookingTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.flight_booking_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/flight_booking_transaction/FlightBookingTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.flight_booking_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
